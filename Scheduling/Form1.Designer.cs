6a1f5d8f044a9f1bde353c4472e450b06b0978e2
User <User@DESKTOP-SBOMTLG>
2024-04-15 21:28:53 +0200

    Aggiungere i file di progetto.

---
+++ Scheduling/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Scheduling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.btnEsegui = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBoxScheduling = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.AutoSize = true;
+            this.btnInserisci.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserisci.Location = new System.Drawing.Point(246, 15);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(77, 30);
+            this.btnInserisci.TabIndex = 0;
+            this.btnInserisci.Text = "Inserisci";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(12, 22);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.Location = new System.Drawing.Point(12, 58);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(62, 20);
+            this.lblTempo.TabIndex = 2;
+            this.lblTempo.Text = "Tempo:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(104, 19);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 26);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempo.Location = new System.Drawing.Point(104, 55);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(100, 26);
+            this.txtTempo.TabIndex = 4;
+            // 
+            // btnEsegui
+            // 
+            this.btnEsegui.AutoSize = true;
+            this.btnEsegui.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEsegui.Location = new System.Drawing.Point(246, 53);
+            this.btnEsegui.Name = "btnEsegui";
+            this.btnEsegui.Size = new System.Drawing.Size(77, 30);
+            this.btnEsegui.TabIndex = 5;
+            this.btnEsegui.Text = "Esegui";
+            this.btnEsegui.UseVisualStyleBackColor = true;
+            this.btnEsegui.Click += new System.EventHandler(this.btnEsegui_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(7, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(599, 144);
+            this.listBox1.TabIndex = 6;
+            // 
+            // comboBoxScheduling
+            // 
+            this.comboBoxScheduling.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxScheduling.FormattingEnabled = true;
+            this.comboBoxScheduling.Items.AddRange(new object[] {
+            "First Come First Served",
+            "Shortest Job First",
+            "Round Robin"});
+            this.comboBoxScheduling.Location = new System.Drawing.Point(416, 22);
+            this.comboBoxScheduling.Name = "comboBoxScheduling";
+            this.comboBoxScheduling.Size = new System.Drawing.Size(190, 24);
+            this.comboBoxScheduling.TabIndex = 7;
+            this.comboBoxScheduling.Text = "Inserisci la politica da usare";
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(246, 89);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(77, 30);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 311);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.comboBoxScheduling);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEsegui);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnInserisci);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.Button btnEsegui;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBoxScheduling;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Scheduling/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Scheduling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<job> processo = new List<job>();
+        int i = 0;
+
+        private void btnInserisci_Click(object sender, EventArgs e)
+        {
+            string nome = "";
+            int tempo = 0;
+            processo.Add(new job());
+            try
+            {
+                tempo = Convert.ToInt32(txtTempo.Text);
+                nome = Convert.ToString(txtNome.Text);
+                processo[i].nome = nome;
+                processo[i].tempo = tempo;
+                listBox1.Items.Add("Nome:  " + processo[i].nome + "          Tempo:  " + processo[i].tempo + "ms");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore  inserimento job: "+ ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtNome.Clear();
+                txtTempo.Clear();
+                processo.RemoveAt(i);
+
+            }
+
+
+            
+            txtNome.Clear();
+            txtTempo.Clear();
+            i++;
+        }
+
+        private async void btnEsegui_Click(object sender, EventArgs e)
+        {
+            switch (comboBoxScheduling.SelectedIndex)
+            {
+                case 0:
+                    listBox1.Items.Clear();
+                    int tempoEsecuzione = 0;
+                    for (int j=0;j<processo.Count;j++)
+                    {
+                        tempoEsecuzione += processo[j].tempo;
+                        Thread.Sleep(processo[j].tempo);
+                        listBox1.Items.Add("Processo:  "+processo[j].nome+ "     tempo: "+ processo[j].tempo + "ms    eseguito con successo");
+                        
+                    }
+                    
+                    listBox1.Items.Add("Processi eseguiti in " + tempoEsecuzione + "ms");
+                    i = 0;
+                    processo.Clear();
+                    break;
+                case 1:
+                    listBox1.Items.Clear();
+                    int varAusilTempo = processo[0].tempo;
+                    string varAusilNome = processo[0].nome;
+                    tempoEsecuzione = 0;
+                    for (int j = 0; j < processo.Count; j++)
+                    {
+                        tempoEsecuzione += processo[j].tempo;
+                        Thread.Sleep(processo[j].tempo);
+                    }
+                    while (processo.Count > 0)
+                    {
+                        //INIZIALIZZA k A PROCESSIINSERITI.COUNT-1 PERCHE' GLI INDICI VANNO FINO A N-1
+                        int k = processo.Count - 1;
+                        for (int j = 0; j < processo.Count-1; j++)
+                        {
+                            if (processo[j + 1].tempo > processo[j].tempo)
+                            {
+                                //ORDINA LA LISTA DAL JOB CON TEMPO PIù GRANDE AL PIU' PICCOLO
+                                varAusilNome = processo[j].nome;
+                                varAusilTempo = processo[j].tempo;
+                                processo[j].tempo = processo[j + 1].tempo;
+                                processo[j].nome = processo[j + 1].nome;
+                                processo[j + 1].nome = varAusilNome;
+                                processo[j + 1].tempo = varAusilTempo;
+                            }
+                        }
+                        //STAMPA L'ULTIMO ELEMENTO DELLA LISTA A CUI E' ASSEGNATO IL JOB CON TEMPO MINORE, RIMUOVE L'OGGETTO PERCHE' NON SERVE PIU' E DECREMENTA  LA i VISTO CHE SI HA UN JOB IN MENO
+                        listBox1.Items.Add("Processo " + processo[k].nome + " eseguito con successo in " + processo[k].tempo + "ms");
+                        i--;
+                        processo.RemoveAt(k); // Rimuovi l'elemento dopo aver stampato il messaggio
+                    }
+
+                    listBox1.Items.Add("Processi eseguiti in " + tempoEsecuzione + "ms");
+                    break;
+
+                
+                case 2:
+                    listBox1.Items.Clear();
+                    tempoEsecuzione = 0;
+                    for (int j = 0; j < processo.Count; j++)
+                    {
+                        tempoEsecuzione += processo[j].tempo;
+                        Thread.Sleep(processo[j].tempo);
+                    }
+                    int timeSlice = 20;
+                    //Cicla finche ci sono elemnti nella lista
+                    while (processo.Count>0)
+                    {
+                        for(int j=0;j<processo.Count;j++)
+                        {
+
+                            //Aggiorna tempo rimanente del processo
+                            processo[j].tempo -= timeSlice;
+
+                            //Rimuovi il procecsso se completato
+                            if (processo[j].tempo<=0)
+                            {
+                                listBox1.Items.Add("Il processo " + processo[j].nome + " è stato completato");
+                                processo.RemoveAt(j);
+                                j--;
+                            }
+                            else
+                            {
+                                listBox1.Items.Add("Il processo " + processo[j].nome + " è stato sospeso");
+
+                            }
+
+                        }
+
+                    }
+                    listBox1.Items.Add("Processi eseguiti in " + tempoEsecuzione + "ms");
+                    break;
+                default:
+                    MessageBox.Show("Inserire la politica da usare","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
